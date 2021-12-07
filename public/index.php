df8917cf95e8f8ceee548a1425220df80eb8edb2
VladimirBH <58952310+VladimirBH@users.noreply.github.com>
2021-12-07 20:33:36 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,22 +1,7 @@
 <?php
     $filepath_ip = '../static/ip_cli.txt';
     $filepath_coord = '../static/car_coord.txt';
-if(!(isset($GET_["x"]) && isset($GET_["y"])))
-{
-    $file_out = file($filepath_coord);
-    $line = $file_out[count($file_out)-1];
-    $numbers = explode(', ', $line);
-    if(count($numbers) < 2)
-    {
-        die("Данных нет");
-    }
-    echo "x=".$numbers[0]. ", y=".$numbers[1]; 
-    if((strtotime(date('Y-m-d H:i:s'))-strtotime(filectime($filepath_ip)) > 10))
-    {
-        die("        Данные не актуальны");
-    }
-}
-else
+if(isset($GET_["x"]) && isset($GET_["y"]))
 {
     $ip_client = $_SERVER['HTTP_HOST'];
     if(filesize($filepath_ip) == 0) //(strtotime(date('Y-m-d H:i:s'))-strtotime(filectime($filepath_ip)) > 30) )
@@ -33,6 +18,21 @@ else
     }
     file_put_contents($filepath_coord,  $_GET["x"]. ', '. $_GET["y"] . PHP_EOL , FILE_APPEND);
 
+}
+else
+{
+    $file_out = file($filepath_coord);
+    $line = $file_out[count($file_out)-1];
+    $numbers = explode(', ', $line);
+    if(count($numbers) < 2)
+    {
+        die("Данных нет");
+    }
+    echo "x=".$numbers[0]. ", y=".$numbers[1]; 
+    if((strtotime(date('Y-m-d H:i:s'))-strtotime(filectime($filepath_ip)) > 10))
+    {
+        die("        Данные не актуальны");
+    }
 }
 
 ?>
