9a096167d7beb5eaf1cb0b169241b99d8d2634a4
VladimirBH <58952310+VladimirBH@users.noreply.github.com>
2021-12-07 17:20:17 +0300

    Add files via upload
    Тестируем

--- public/index.php
+++ public/index.php
@@ -1,5 +1,25 @@
 <?php
+$filepath_ip = '../static/ip_cli.txt';
+$filepath_coord = '../static/car_coord.txt';
 
-phpinfo();
-
+$ip_client = $_SERVER['HTTP_HOST'];
+if(filesize($filepath_ip) == 0 || filectime($filepath_ip))
+{
+    $fp = fopen($filepath_ip, 'w');
+    fputs($fp, $ip_client);
+    fclose($fp);
+}
+else
+{
+    $fp = fopen($filepath_ip, 'r');
+    if($ip_client != fgets($fp))
+    {
+        fclose($fp);
+        die("UserErrorSecurity");
+    }
+    fclose($fp);
+}
+//$file_in =fopen($filepath_coord, 'w');
+file_put_contents($filepath_coord,  PHP_EOL . $_GET["x"]. ', '. $_GET["y"], FILE_APPEND);
+//fclose($file_in);
 ?>
