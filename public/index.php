7bb5559a87303a8e6dfc0d28013a50cd12dc872e
VladimirBH <58952310+VladimirBH@users.noreply.github.com>
2021-12-07 20:24:22 +0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,16 +1,21 @@
 <?php
-if(isset($GET_["x"]) && isset($GET_["y"]))
+    $filepath_ip = '../static/ip_cli.txt';
+    $filepath_coord = '../static/car_coord.txt';
+if(!(isset($GET_["x"]) && isset($GET_["y"])))
 {
-
-
+    $file_out = file($filepath_coord);
+    $line = $file_out[count($file_out)-1];
+    $numbers = explode(', ', $line);
+    echo "x=".$numbers[0]. ", y=".$numbers[1]; 
+    if((strtotime(date('Y-m-d H:i:s'))-strtotime(filectime($filepath_ip)) > 10))
+    {
+        die("Данные не актуальны");
+    }
 }
 else
 {
-    $filepath_ip = '../static/ip_cli.txt';
-    $filepath_coord = '../static/car_coord.txt';
-
     $ip_client = $_SERVER['HTTP_HOST'];
-    if(filesize($filepath_ip) == 0 || (strtotime(date('Y-m-d H:i:s'))-strtotime(filectime($filepath_ip)) > 30) )
+    if(filesize($filepath_ip) == 0) //(strtotime(date('Y-m-d H:i:s'))-strtotime(filectime($filepath_ip)) > 30) )
     {
         file_put_contents($filepath_ip, $ip_client);
     }
